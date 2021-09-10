4deaa639cd2e866e9b34c4927ef377edcffd2d0a
Promise-nuwasa <promisenuwasasira@gmail.com>
2021-09-10 19:03:24 +0300

    save arezzo files

--- schoolsystem/api/urls.py
+++ schoolsystem/api/urls.py
@@ -1,9 +1,15 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import StudentViewSet
+from .views import StudentViewSet,TeacherViewSet,CoursesViewSet,EventViewSet
 
 router = routers.DefaultRouter()
 router.register(r"student", StudentViewSet)
+router.register(r"teacher", TeacherViewSet)
+router.register(r"courses", CoursesViewSet)
+router.register(r"cal", EventViewSet)
+
+
+
 
 urlpatterns = [
     path("", include(router.urls))
